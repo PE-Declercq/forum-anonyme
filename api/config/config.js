8542bcc7c0faaff327abfa728dbbff645847c450
PE-Declercq <82029636+PE-Declercq@users.noreply.github.com>
2024-04-19 11:41:14 +0200

    fix: update config.js

--- api/config/config.js
+++ api/config/config.js
@@ -2,7 +2,7 @@ const config = {
     db: {
       host: process.env.DB_HOST || 'localhost',
       user: process.env.DB_USER || 'root',
-      password: process.env.DB_PASSWORD || 'password',
+      password: process.env.DB_PASSWORD || 'root',
       database: process.env.DB_NAME || 'forum',
       port: process.env.DB_PORT || 3306
     },

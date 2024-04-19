296eafc4a32ddd0b1d6e54222b25a1d0bbf64ea7
PE-Declercq <82029636+PE-Declercq@users.noreply.github.com>
2024-04-19 12:09:02 +0200

    fix: update docker-compose, package.json & modules for build

--- api/config/config.js
+++ api/config/config.js
@@ -1,6 +1,6 @@
 const config = {
     db: {
-      host: process.env.DB_HOST || 'localhost',
+      host: process.env.DB_HOST || 'db-1',
       user: process.env.DB_USER || 'root',
       password: process.env.DB_PASSWORD || 'root',
       database: process.env.DB_NAME || 'forum',

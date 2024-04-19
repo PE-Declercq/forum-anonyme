4d1496fa523cb814d949fa98a12b433efc4a7ba6
PE-Declercq <82029636+PE-Declercq@users.noreply.github.com>
2024-04-19 12:21:46 +0200

    feat: adding db init

--- api/config/config.js
+++ api/config/config.js
@@ -1,8 +1,8 @@
 const config = {
     db: {
       host: process.env.DB_HOST || 'db-1',
-      user: process.env.DB_USER || 'root',
-      password: process.env.DB_PASSWORD || 'root',
+      user: process.env.DB_USER || 'user',
+      password: process.env.DB_PASSWORD || 'password',
       database: process.env.DB_NAME || 'forum',
       port: process.env.DB_PORT || 3306
     },
